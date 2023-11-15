67c9a2522df08110470846358ea315bcc08ba227
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-11-15 21:25:48 +0900

    add/결과 페이지 코멘트 불러오기

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,11 @@ router.get("/test2023", controller.testStart);
 // // 결과 보기
 router.post("/test2023", controller.getResult);
 
-// router.get("/result", (req, res) => {
-//   res.render("result");
-// });
+// 결과창 이동
+router.get("/result", controller2.comments);
 
 // 댓글조회
-router.post("/comments", controller2.comments);
+// router.post("/comments", controller2.comments);
 
 // 댓글 등록
 router.post("/comment", controller2.createComments);
