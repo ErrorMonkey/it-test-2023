e65b4e2563796588c4230061a32840039af5cfca
9walnut <kguho9202@gmail.com>
2023-11-15 13:01:36 +0900

    fix: test & result page modified

--- utils/resultValue.js
+++ utils/resultValue.js
@@ -1,25 +1,15 @@
-const resultName = ["똥바보", "바보", "일반인", "똑똑해", "천재"];
-const resultText = ["0<점수<20에 대한 설명","20<점수<40에 대한 설명","40<점수<60에 대한 설명","60<점수<80에 대한 설명","80<점수<100에 대한 설명"];
-
 function getResultInfo(score) {
-  let index;
-
-  if (score >= 0 && score < 20) {
-    index = 0;
-  } else if (score >= 20 && score < 40) {
-    index = 1;
-  } else if (score >= 40 && score < 60) {
-    index = 2;
-  } else if (score >= 60 && score < 80) {
-    index = 3;
+  if (score >= 0 && score <= 20) {
+    return "rank5.jpg"; // 0~20점 범위의 이미지 파일
+  } else if (score > 20 && score <= 40) {
+    return "rank4.jpg"; // 21~40점 범위의 이미지 파일
+  } else if (score > 40 && score <= 60) {
+    return "rank3.jpg"; // 41~60점 범위의 이미지 파일
+  } else if (score > 60 && score <= 80) {
+    return "rank2.jpg"; // 61~80점 범위의 이미지 파일
   } else {
-    index = 4;
+    return "rank1.jpg"; // 81~100점 범위의 이미지 파일
   }
-
-  return {
-    name: resultName[index],
-    text: resultText[index]
-  };
 }
 
 module.exports = getResultInfo;
