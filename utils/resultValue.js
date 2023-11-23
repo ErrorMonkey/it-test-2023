361adf598dabca41ae4433ea3d6bb93ba170cf11
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-11-23 18:11:35 +0900

    fix: resultValue.js 필요 없는 정보 삭제

--- utils/resultValue.js
+++ utils/resultValue.js
@@ -2,44 +2,29 @@ function getResultInfo(score) {
   if (score >= 0 && score <= 20) {
     return {
       grade: "5",
-      gradeTitle: "좀 치시네요",
-      desc: "잘 치시네요",
-      img: "../public/image/grade5.png",
+      img: "/public/image/grade5.png",
     };
   } else if (score > 20 && score <= 40) {
     return {
       grade: "4",
-      gradeTitle: "4444",
-      desc: "잘 치시네요",
-      img: "../public/image/grade4.png",
+      img: "/public/image/grade4.png",
     };
   } else if (score > 40 && score <= 60) {
     return {
       grade: "3",
-      gradeTitle: "33333",
-      desc: "잘 치시네요",
-      img: "../public/image/grade3.png",
+      img: "/public/image/grade3.png",
     };
   } else if (score > 60 && score <= 80) {
     return {
       grade: "2",
-      gradeTitle: "22222222",
-      desc: "잘 치시네요",
-      img: "../public/image/grade2.png",
+      img: "/public/image/grade2.png",
     };
   } else {
     return {
       grade: "1",
-      gradeTitle: "좀 치시네요",
-      desc: "잘 치시네요",
-      img: "../public/image/grade1.png",
+      img: "/public/image/grade1.png",
     };
   }
 }
 
-// export default getResultInfo;
-
 module.exports = getResultInfo;
-
-// 백엔드 commonJS
-// 프론트엔드 ES6
